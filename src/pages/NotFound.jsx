21105bafe51002a53528d3349f08fa841bb84db1
Patrickodida <patrick.o.odida@gmail.com>
2025-08-14 19:45:00 +0300

    Add a 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-
-const NotFound = () => {
+export default function NotFound() {
   return (
-    <div>NotFound</div>
-  )
+    <div className="max-w-3xl mx-auto px-4 py-16 text-center">
+      <h1 className="text-3xl font-bold">404</h1>
+      <p className="text-gray-600 mt-2">Page not found.</p>
+    </div>
+  );
 }
-
-export default NotFound
