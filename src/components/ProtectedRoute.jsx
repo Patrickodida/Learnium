8432f3b6d4eb5041de0048c60e3b0bd3ec92477a
Patrickodida <patrick.o.odida@gmail.com>
2025-08-14 18:05:07 +0300

    Implement user access after login(Protected route)

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,10 +1,10 @@
 import React from "react";
+import { Navigate  } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
-const ProtectedRoute = () => {
-    return (
-        <div>
-            <h2 className="text-2l text-orange-500">Protected Route</h2>
-        </div>
-    )
-}
-export default ProtectedRoute;
+export default function ProtectedRoute({ children }) {
+    const { user } = useContext(AuthContext);
+    if(!user) return <Navigate to="/login" replace />
+    return children;
+};
