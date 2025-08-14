e0542ffb23a99137cd1a04dddfec9378a245f89b
Patrickodida <patrick.o.odida@gmail.com>
2025-08-14 18:19:29 +0300

    Implement Initiating payment with a start payment button.

--- src/components/PaymentButton.jsx
+++ src/components/PaymentButton.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { initiatePayment } from "../services/paymentService";
 
-const PaymentButton = () => {
-    return (
-        <div>
-            <h2 className="text-2l text-red-500">Payment Button</h2>
-        </div>
-    )
-}
+const PaymentButton = ({ amount, currency = "UGX", courseId }) => {
+  const [loading, setLoading] = useState(false);
+
+  const startPayment = async () => {
+    try {
+      setLoading(true);
+      const { paymentLink } = await initiatePayment(amount, currency, courseId);
+      window.location.href = paymentLink; // Redirect to Paystack checkout
+    } catch (err) {
+      alert(err?.response?.data?.error || "Failed to start payment");
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <button
+      disabled={loading}
+      onClick={startPayment}
+      className="w-full py-2 rounded bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-60"
+    >
+      {loading
+        ? "Starting..."
+        : `Buy Now . ${amount.toLocalString()} ${currency}`}
+    </button>
+  );
+};
 export default PaymentButton;
