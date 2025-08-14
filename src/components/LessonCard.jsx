4333ea4c3dd192be137f6e6175d6eb44aeadd03f
Patrickodida <patrick.o.odida@gmail.com>
2025-08-14 17:57:49 +0300

    Implement user accessing lessons after accessing the course.

--- src/components/LessonCard.jsx
+++ src/components/LessonCard.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const LessonCard = () => {
-    return (
-        <div>
-            <h2 className="text-2l text-yellow-500">Lesson Card</h2>
-        </div>
-    )
-}
+const LessonCard = ({ lesson }) => {
+  return (
+    <Link
+      to={`/lessons/${lesson.id}`}
+      className="block border rounded p-3 hover:bg-gray-50"
+    >
+      <div className="font-medium">{lesson.title}</div>
+      <div className="text-xs text-gray-500">Position: {lesson.position}</div>
+    </Link>
+  );
+};
 export default LessonCard;
